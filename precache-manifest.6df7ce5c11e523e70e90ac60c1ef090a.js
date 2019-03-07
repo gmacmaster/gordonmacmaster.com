c8088b4830da40e547757755f967282a6660ac00
gmacmaster <gmacmast@uvm.edu>
2019-03-06 18:49:16 -0800

    Updates

---
+++ precache-manifest.6df7ce5c11e523e70e90ac60c1ef090a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a62ba19e32170301e48719dd77b91f1",
+    "url": "/static/media/wordcloud.4a62ba19.png"
+  },
+  {
+    "revision": "94e9470d6de31e964caa959af1cb42d7",
+    "url": "/static/media/me.94e9470d.jpg"
+  },
+  {
+    "revision": "0f38ba389648e650c988b770a42d1a59",
+    "url": "/static/media/logo.0f38ba38.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "68b38be9c92ed0391942",
+    "url": "/static/js/main.68b38be9.chunk.js"
+  },
+  {
+    "revision": "d03cbe37b4073ff8153f",
+    "url": "/static/js/2.d03cbe37.chunk.js"
+  },
+  {
+    "revision": "68b38be9c92ed0391942",
+    "url": "/static/css/main.484c0638.chunk.css"
+  },
+  {
+    "revision": "d03cbe37b4073ff8153f",
+    "url": "/static/css/2.9209d1df.chunk.css"
+  },
+  {
+    "revision": "b344f122c89d921ed7e0295de90e2247",
+    "url": "/index.html"
+  }
+];

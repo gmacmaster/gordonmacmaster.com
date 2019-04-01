09177d2f54c1abc6f31e81ac24ffcb5c1c64062d
Gordon D. MacMaster <gordon.macmaster@uvm.edu>
2019-04-01 00:28:37 -0700

    fixed typos

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -14,7 +14,7 @@ class AboutSection extends Component {
                         I Am A Full Stack Developer
                     </h1>
                     <p className="aboutParagraph">My name is Gordon MacMaster. I am a junior computer science and information science major at the University of Vermont. I have six years
-                        of programing experience in various languages and frameworks. I have interned with the Vermont Agency of Digital Services, Pennsylvania Multifamily
+                        of programming experience in various languages and frameworks. I have interned with the Vermont Agency of Digital Services, Pennsylvania Multifamily
                         Asset Managers, and am currently interning for Tesla Motors as a Digital Products Engineer. I am interested in all aspects of computer science from
                         front end design to machine learning and everything in between.
                     </p>
